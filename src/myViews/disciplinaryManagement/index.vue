9b05a3265505932fb92a51aed6e785ecf94dbd10
lhzandyqh <yangqihang822@gmail.com>
2021-01-18 15:45:47 +0800

    Initial commit

---
+++ src/myViews/disciplinaryManagement/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="app-container">
+    <h1>这是违纪处分管理</h1>
+    <div class="myfirsttable">
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+      >
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180"
+        />
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180"
+        />
+        <el-table-column
+          prop="address"
+          label="地址"
+        />
+        <el-table-column
+          prop="detail"
+          label="详细情况"
+        />
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        detail: '1111111'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+        detail: '1111111'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+        detail: '1111111'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        detail: '1111111'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/myViews/test/oneIndex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <h1>两级导航第一个index</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OneIndex'
+}
+</script>
+
+<style scoped>
+
+</style>
