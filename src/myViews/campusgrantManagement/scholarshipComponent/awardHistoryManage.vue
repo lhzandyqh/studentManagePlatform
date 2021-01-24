144ae1325b201a83d366386f6f34a8f64077ba0a
lhzandyqh <yangqihang822@gmail.com>
2021-01-24 14:16:12 +0800

    20200124修改

---
+++ src/myViews/campusgrantManagement/scholarshipComponent/awardHistoryManage.vue
@@ -0,0 +1,259 @@
+<template>
+  <div class="app-container">
+    <div class="button_head clearfix">
+      <div class="search_container">
+        <el-select v-model="type" placeholder="请选择年度">
+          <el-option label="2020" value="shanghai" />
+          <el-option label="2019" value="beijing" />
+          <el-option label="2018" value="beijing" />
+          <el-option label="2017" value="beijing" />
+          <el-option label="2016" value="beijing" />
+        </el-select>
+        <el-button type="primary">搜索</el-button>
+      </div>
+    </div>
+    <el-divider />
+    <div class="table_container">
+      <el-table
+        :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+        stripe
+        style="width: 100%"
+      >
+        <el-table-column
+          prop="date"
+          label="编号"
+          width="100"
+        />
+        <el-table-column
+          prop="奖学金名称"
+          label="姓名"
+        />
+        <el-table-column
+          prop="address"
+          label="年度"
+        />
+        <el-table-column
+          prop="address"
+          label="级别"
+        />
+        <el-table-column
+          prop="address"
+          label="申请开始日期"
+        />
+        <el-table-column
+          prop="address"
+          label="申请结束日期"
+        />
+        <el-table-column
+          label="操作"
+        >
+          <template slot-scope="scope">
+            <el-button type="text" @click="getScholarshipDetail(scope.row)">查看获奖人员</el-button>
+            <el-button type="text">导出</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="fenye">
+        <el-pagination
+          :current-page="currentPage"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="tableData.length"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
+    </div>
+    <el-dialog
+      title="获奖人员"
+      :visible.sync="dialogVisible"
+      width="50%"
+      :before-close="handleClose"
+    >
+      <el-table
+        :data="tableDataTwo.slice((currentPage2-1)*pageSize,currentPage2*pageSize)"
+        style="width: 100%"
+      >
+        <el-table-column
+          prop="date"
+          label="姓名"
+          width="180"
+        />
+        <el-table-column
+          prop="name"
+          label="学号"
+          width="180"
+        />
+        <el-table-column
+          prop="address"
+          label="专业"
+        />
+        <el-table-column
+          prop="address"
+          label="获奖日期"
+        />
+      </el-table>
+      <div class="fenye">
+        <el-pagination
+          small
+          :current-page="currentPage2"
+          page-size="5"
+          layout="prev, pager, next"
+          :total="tableDataTwo.length"
+          @current-change="handleCurrentChangeTwo"
+        />
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AwardHistoryManage',
+  data() {
+    return {
+      dialogVisible: false,
+      type: '',
+      currentPage: 1,
+      currentPage2: 1,
+      pageSize: 5,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      tableDataTwo: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }]
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    handleSizeChange(val) {
+      this.currentPage = 1
+      this.pageSize = val
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val
+    },
+    handleCurrentChangeTwo(val) {
+      this.currentPage2 = val
+    },
+    // 自己的方法
+    getScholarshipDetail: function() {
+      this.dialogVisible = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search_container{
+  float: left;
+}
+.button_container{
+  float: right;
+  padding-right: 100px;
+}
+.main_title span {
+  font-size: 15px;
+  font-weight: bold;
+  color: #666666;
+}
+.table_container{
+  margin-top: 20px;
+}
+.fenye {
+  text-align: center;
+  margin-top: 20px;
+}
+.clearfix :after{
+  display:block;
+  height:0;
+  clear:both;
+  visibility: hidden;
+  overflow:hidden;
+}
+.clearfix{
+  zoom: 1;
+}
+.audit_title {
+  font-weight: bold;
+}
+.foot{
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
--- src/myViews/campusgrantManagement/scholarshipManagement.vue
+++ src/myViews/campusgrantManagement/scholarshipManagement.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>这是奖学金管理页面</div>
+  <div class="app-container">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="奖学金信息管理" name="first">
+        <info-manage />
+      </el-tab-pane>
+      <el-tab-pane label="奖学金申请审核" name="second">
+        <reply-audit />
+      </el-tab-pane>
+      <el-tab-pane label="奖学金获奖历史管理" name="third">
+        <award-history-manage />
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script>
+import infoManage from './scholarshipComponent/infoManage'
+import replyAudit from './scholarshipComponent/replyAudit'
+import awardHistoryManage from './scholarshipComponent/awardHistoryManage'
 export default {
-  name: 'ScholarshipManagement'
+  name: 'ScholarshipManagement',
+  components: { infoManage, replyAudit, awardHistoryManage },
+  data() {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    }
+  }
 }
 </script>
 
