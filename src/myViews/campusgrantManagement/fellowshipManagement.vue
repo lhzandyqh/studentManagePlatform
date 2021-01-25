f4d508d525a12ab756433e7803dd14f082ccf812
Squirrel152 <1204312973@qq.com>
2021-01-25 14:30:18 +0800

    20210125lixian

--- src/myViews/campusgrantManagement/fellowshipManagement.vue
+++ src/myViews/campusgrantManagement/fellowshipManagement.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>这是助学金管理页面</div>
+  <div class="app-container">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="助学金申请审核" name="first">
+        <fellow-reply />
+      </el-tab-pane>
+      <el-tab-pane label="助学金获奖历史管理" name="second">
+        <fellow-history />
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script>
+import fellowReply from './fellowshipComponent/fellowReply'
+import fellowHistory from './fellowshipComponent/fellowHistory'
 export default {
-  name: 'FellowshipManagement'
+  name: 'FellowshipManagement',
+  components: { fellowReply, fellowHistory },
+  data() {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 </style>
+
